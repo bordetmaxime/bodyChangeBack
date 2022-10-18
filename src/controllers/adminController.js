585870b502a03a094818a28ced4177339ff194d8
Bordet Maxime <maxime.bordet83@gmail.com>
2022-10-18 15:40:01 +0200

    ok

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -6,51 +6,56 @@ const bcrypt = require('bcrypt');
 
 const adminController = {
 
-  getUser : async (req,res) => {
-  
-email = req.body.email;
-password = req.body.password;
-adminSearch = await Admin.findOne({where : {admin_email: email}})
-goodPassword = await bcrypt.compare(password, adminSearch.admin_password);
+  getUser : async (req,res,next) => {
+   try{
+const email = req.body.email;
+const password = req.body.password;
+
+const adminSearch = await Admin.findOne({where : {admin_email: email}})
+const goodPassword = await bcrypt.compare(password, adminSearch.admin_password);
 
   if(goodPassword){
-  res.json(adminSearch)}
-
+  res.json(adminSearch)
+}
 
 
 else {
   res.status(404).send(`Cant find list with id ${adminId}`);
 }
-},
+} catch (err) {
+next(err);
+}},
 
 
 
       
     createOrModify: async (req, res, next) => {
-        
-        const hashPassword = await bcrypt.hash(req.body.password, 10);
+        try {
+
+          const hashPassword = await bcrypt.hash(req.body.password, 10);
              
-        Admin.create({
+        await Admin.create({
             admin_firstname: req.body.firstname,
             admin_lastname: req.body.lastname,
             admin_email: req.body.email,
             admin_password: hashPassword,
           });
   
-
-
         res.json("okey");
       
-  
+   
+      } catch (err) {
+        next(err);
       }
     
-              };
+              },
 
    
+  
                             
 
 
-                          
+                            };
                         
     
     
